08da385417aca041d2eb7d18a68b98477861046e
XavierKervyn <65119223+XavierKervyn@users.noreply.github.com>
2021-03-25 11:18:55 +0100

    osef

--- Exercice6.cpp
+++ Exercice6.cpp
@@ -226,8 +226,8 @@ int main(int argc, char* argv[])
   {
     // TODO: Calculer div(E_r) et div(D_r)/epsilon_0
     // en utilisant des diff�rences finies centr�es aux milieux des milieux des intervalles
-	div_Er[i] = 1.0 / rmidmid[i-1] * ( ( rmid[i] * Er[i] - rmid[i-1] * Er[i-1] ) / ( rmid[i] - rmid[i-1] ) );
-  div_Dr[i] = 1.0 / rmidmid[i-1] * ( ( rmid[i] * Dr[i] - rmid[i-1] * Dr[i-1] ) / ( rmid[i] - rmid[i-1] ) );
+	div_Er[i-1] = 1.0 / rmidmid[i-1] * ( ( rmid[i] * Er[i] - rmid[i-1] * Er[i-1] ) / ( rmid[i] - rmid[i-1] ) );
+  div_Dr[i-1] = 1.0 / rmidmid[i-1] * ( ( rmid[i] * Dr[i] - rmid[i-1] * Dr[i-1] ) / ( rmid[i] - rmid[i-1] ) );
   //div_Dr[i]= 1/rmidmid[i] * (Dr[i] + rmidmid[i]* (Dr[i+1]-Dr[i])/(rmidmid[i+1]-rmidmid[i]));
   }
   ofs.open(configFile.get<string>("outputDivergences").c_str());
